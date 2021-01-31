0fbb8f06476c8b35083fd6de3fc2f0e9a413c99a
Gideon Tong <gideon@gideontong.com>
2021-01-30 16:56:14 -0800

    Add crypto donation options

---
+++ donate/copy.js
@@ -0,0 +1,25 @@
+address = {
+    btc: 'bc1qzdjcskxtylm0ujz5xxqgzdn504dhkh2ml4g2yx',
+    eth: '0x5a08a4110283E4D8F2adDDaC603CC4f06397b5D8'
+}
+
+function copy(type) {
+    var input = document.createElement('input');
+    if (type in address) {
+        input.value = address[type];
+    } else {
+        input.value = address['btc'];
+    }
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+}
+
+document.querySelector('#btc').onclick = function() {
+    copy('btc');
+}
+
+document.querySelector('#eth').onclick = function() {
+    copy('eth');
+}
